79f7889433d99dec4e721de1d359d967a84bcda5
Jax-Hax <jaxhacks543@gmail.com>
2023-09-30 16:11:35 -0400

    added some basic node details and functions and stuff

--- src/ast.rs
+++ src/ast.rs
@@ -5,3 +5,28 @@ use crate::lexer::Token;
 pub fn construct_tree(mut tokens: IntoIter<Token>) {
     let token = tokens.next().unwrap();
 }
+pub enum Node {
+    Val(f32),
+    UnaryExpr {
+        op: char,
+        child: Box<Node>,
+    },
+    BinaryExpr {
+        op: char,
+        lhs: Box<Node>,
+        rhs: Box<Node>,
+    },
+    FunctionCall {
+        name: String,
+        args: Vec<Box<Node>>,
+    },
+    Function {
+        params: FunctionParams,
+        args: Vec<Box<Node>>,
+    },
+    Variable(String),
+}
+pub struct FunctionParams {
+    pub name: String,
+    pub args: Vec<String>,
+}
