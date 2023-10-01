68bfa27e89f1fa02db73de6f346f0d3bf28d51b9
Jax-Hax <jaxhacks543@gmail.com>
2023-10-01 15:57:33 -0400

    trying to add some expressions to parse things but an ast is complex

--- src/ast.rs
+++ src/ast.rs
@@ -3,7 +3,52 @@ use std::vec::IntoIter;
 use crate::lexer::Token;
 
 pub fn construct_tree(mut tokens: IntoIter<Token>) {
+    let mut token = tokens.next().unwrap();
+    loop {
+        
+        token = tokens.next().unwrap();
+    }
+}
+fn match_token(cur_token: &mut Token, tokens: &mut IntoIter<Token>) -> NodeReturn {
+    match cur_token {
+        Token::Eof => NodeReturn::Eof,
+        Token::Fn => todo!(),
+        Token::Identifier(_) => todo!(),
+        Token::Number(num) => NodeReturn::Node(Node::Val(num)),
+        Token::Other(char) => {
+            if char == &mut '(' {
+
+            }
+        },
+    }
+}
+fn match_char(char: &mut char, tokens: &mut IntoIter<Token>) {
+    if char == &mut '(' {
+
+    }
+}
+fn parse_parenthesis(tokens: &mut IntoIter<Token>) {
     let token = tokens.next().unwrap();
+}
+fn parse_expr() {
+
+}
+fn parse_identifier_expr(identifier: String, tokens: &mut IntoIter<Token>) -> Node{ //called when the current token is an identifier token
+    let token = tokens.next().unwrap(); //eat identifier
+    match token {
+        Token::Other(char) => if char != '(' {
+            return Node::Variable(identifier)
+        }
+        _ => {}
+    }
+    if token != '(' {
+        return Node::Variable(token)
+    }
+}
+enum NodeReturn{
+    Node(Node),
+    Eof,
+    Nothing,
 }
 pub enum Node {
     Val(f32),
@@ -22,7 +67,7 @@ pub enum Node {
     },
     Function {
         params: FunctionParams,
-        args: Vec<Box<Node>>,
+        body: Box<Node>,
     },
     Variable(String),
 }
