386b8b18b1ea2ba0c41c7d8700fae517ac191c33
Jax-Hax <jaxhacks543@gmail.com>
2023-10-02 16:17:08 -0400

    added an ast class for easier token grabbing

--- src/ast.rs
+++ src/ast.rs
@@ -1,26 +1,60 @@
 use std::vec::IntoIter;
-
 use crate::lexer::Token;
 
-pub fn construct_tree(mut tokens: IntoIter<Token>) {
-    let mut token = tokens.next().unwrap();
-    loop {
-        
-        token = tokens.next().unwrap();
-    }
+pub struct AST {
+  tokens: IntoIter<Token>,
+  cur_token: Token,
 }
-fn match_token(cur_token: &mut Token, tokens: &mut IntoIter<Token>) -> NodeReturn {
-    match cur_token {
-        Token::Eof => NodeReturn::Eof,
-        Token::Fn => todo!(),
-        Token::Identifier(_) => todo!(),
-        Token::Number(num) => NodeReturn::Node(Node::Val(num)),
-        Token::Other(char) => {
-            if char == &mut '(' {
-
-            }
-        },
+impl AST {
+  pub fn new(mut tokens: IntoIter<Token>) -> Self{
+    let token = tokens.next().unwrap();
+    Self {
+      cur_token: token,
+      tokens,
     }
+  }
+  pub fn next_tok(&mut self) {
+    let token = self.tokens.next().unwrap();
+    self.cur_token = token;
+  }
+  fn parse_tok(&mut self) -> Node {
+    match self.cur_token{
+      
+    }
+  }
+  fn parse_num(num: f32) -> Node {
+    Node::Val(num)
+  }
+  fn parse_primary(&mut self) {
+    match self.cur_token{
+      _ => {eprintln!("Unknown token found when parsing expression: {:#?}", self.cur_token);}
+    }
+  }
+}
+pub fn construct_tree(mut tokens: IntoIter<Token>) -> Vec<Node> {
+  let mut ast = AST::new(tokens);
+  let nodes = vec![];
+  loop {
+    let node = ast.parse_tok();
+    ast.next_tok();
+    if let Node::Eof = node {
+      return nodes;
+    }
+    nodes.push(node);
+  }
+}
+/*fn match_token(cur_token: &mut Token, tokens: &mut IntoIter<Token>) -> NodeReturn {
+  match cur_token {
+      Token::Eof => NodeReturn::Eof,
+      Token::Fn => todo!(),
+      Token::Identifier(_) => todo!(),
+      Token::Number(num) => NodeReturn::Node(Node::Val(num)),
+      Token::Other(char) => {
+          if char == &mut '(' {
+
+          }
+      },
+  }
 }
 fn match_char(char: &mut char, tokens: &mut IntoIter<Token>) {
     if char == &mut '(' {
@@ -51,7 +85,7 @@ fn parse_identifier_expr(identifier: String, tokens: &mut IntoIter<Token>) -> No
         }
     }
     Node::FunctionCall { name: identifier, args: () }
-}
+}*/
 enum NodeReturn{
     Node(Node),
     Eof,
@@ -59,6 +93,7 @@ enum NodeReturn{
 }
 pub enum Node {
     Val(f32),
+    Eof,
     UnaryExpr {
         op: char,
         child: Box<Node>,
